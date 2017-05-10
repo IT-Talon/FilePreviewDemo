0afd6b128959b9b5ed6e11d8ea6b5cc06f94c6b7
unknown <>
2017-05-10 12:49:50 +0800

    Initial commit

---
+++ app/src/main/java/com/example/think/filepreviewdemo/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.think.filepreviewdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView mWebView;
+    private String fileUrl = "http://119.37.192.234:9079/JDZJ/Resource/988202f2-5161-4bb5-a055-b89b328d620b.doc";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mWebView = (WebView) findViewById(R.id.main_webview);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.getSettings().setSupportZoom(true);
+        mWebView.getSettings().setBuiltInZoomControls(true);
+        mWebView.getSettings().setUseWideViewPort(true);
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+//        mWebView.loadUrl("http://docs.google.com/gview?embedded=true&url=" + fileUrl); //google官方预览，需要翻墙
+        mWebView.loadUrl("http://ow365.cn/?i=12428&furl=" + fileUrl); //officeweb365形式预览，付费后可以去掉广告
+    }
+
+}
+
